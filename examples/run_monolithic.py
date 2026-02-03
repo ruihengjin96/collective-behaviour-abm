1293a78931c4ee6cdfabd73bd187d9b5ce0c4c42
ruihengjin96 <ruihengjin1996@gmail.com>
2026-02-04 01:33:38 +0800

    Modify sys.path

--- examples/run_monolithic.py
+++ examples/run_monolithic.py
@@ -1,6 +1,11 @@
 # examples/run_monolithic.py
 
-from src.abmsim.boid_predator_v3 import main  
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
+
+from abmsim.boid_predator_v3 import main  
 
 if __name__ == "__main__":
     main() 
