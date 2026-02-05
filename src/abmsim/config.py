b08e4049f3ffcf49e2afd86f507a574a74c9b463
ruihengjin96 <ruihengjin1996@gmail.com>
2026-02-05 23:59:00 +0800

    Add config and model script

---
+++ src/abmsim/config.py
@@ -0,0 +1,46 @@
+import random
+import math
+import tkinter as tk
+import numpy as np
+
+# -------------------------------
+# CONFIGURATION
+# -------------------------------
+## CANVAS CONFIGURATION
+WIDTH = 800
+HEIGHT = 600
+MARGIN = 100
+
+## BOID CONFIGURATION
+NUM_BOIDS = 200
+BOID_VIS_RANGE = 75
+SPEED_LIMIT = 10
+TURN_FACTOR = 3
+BOID_CENTERING_FACTOR = 0.005
+AVOID_FACTOR = 0.05
+MATCHING_FACTOR = 0.05
+DFLT_AVOID_DIST = 10
+DRAW_TRAIL = False
+DFLT_FLEE_DIST = 100
+FLEE_FACTOR = 1.5
+
+"""Trying to bind some variables to Tkinter widgets
+added a live version of AVOID_DIST to within main()"""
+
+## PREDATOR CONFIGURATION
+NUM_PREDS = 10
+HUNTING_SIGHT = 50
+PRED_COHESION_VIS_RANGE = 100
+PRED_CENTERING_FACTOR = 0.005
+HEARING_RANGE = HUNTING_SIGHT*4
+PRED_AVOID_RADIUS = 50
+PRED_REP_HUNT_FACTOR = 0.2
+PRED_REP_WANDER_FACTOR = 0.5
+CATCH_DIST = 40 # make it big to test if it works
+
+## REFUGE CONFIGURATION -- Defining it as a circular refuge first
+REFUGE_R = 150
+REFUGE_CX = 400
+REFUGE_CY = 300
+REFUGE_REPEL = 20
+REFUGE_BUFFER = 50
