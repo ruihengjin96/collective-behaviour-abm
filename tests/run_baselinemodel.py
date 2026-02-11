23d35133beeffdd498dbc2c51f71e71b9e5b6855
ruihengjin96 <ruihengjin1996@gmail.com>
2026-02-11 22:47:50 +0800

    Move scripts for running simulation to tests\

--- tests/run_baselinemodel.py
+++ tests/run_baselinemodel.py
@@ -5,7 +5,7 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 
-from abmsim.boid_predator_v3 import main  
+from temp.boid_predator_v3 import main  
 
 if __name__ == "__main__":
     main() 
