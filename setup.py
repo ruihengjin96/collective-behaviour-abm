a28bf7d0855b92f36cfe0ef2e527e41c3b3ed476
j.w.jolles@gmai.com <j.w.jolles@gmai.com>
2026-03-18 16:28:04 +0100

    Expand setup file

--- setup.py
+++ setup.py
@@ -2,4 +2,30 @@
 
 from setuptools import setup
 
-setup()
+# Project metadata
+DESCRIPTION = "An interactive toolkit for agent-based modelling"
+DISTNAME = ""
+URL = "https://github.com/ruihengjin96/collective-behaviour-abm"
+DOWNLOAD_URL = "https://github.com/ruihengjin96/collective-behaviour-abm"
+
+# Setup
+if __name__ == "__main__":
+       setup(
+        version="0.0.1",
+        description=DESCRIPTION,
+        url=URL,
+        download_url=DOWNLOAD_URL,
+        license="Apache-2.0",
+        platforms=["Windows", "Linux", "Mac OS-X"],
+        install_requires=[],
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Apache Software License",
+            "Topic :: Scientific/Engineering :: Visualization",
+            "Topic :: Scientific/Engineering :: Information Analysis",
+            "Operating System :: POSIX",
+            "Operating System :: Unix",
+            "Operating System :: MacOS",
+        ],
+       )
